03a3b4a448e0ee62f3aee8b0ae50aade1ac902f6
mf1732047 <hcw960810>
2017-11-13 17:27:51 +0800

    [fix] write log

--- src/main/java/nju/software/util/FileUtil.java
+++ src/main/java/nju/software/util/FileUtil.java
@@ -8,37 +8,62 @@ import java.util.List;
  * Created by SuperSY on 2017/11/7.
  */
 public class FileUtil {
-    public static boolean isBreak = false ;
+    public static boolean isBreak = false;
+    private static String fileLog = "/home/hong/clustering.log";
+    private static FileWriter logFw;
+    private static String fileUser = "/home/hong/user.txt";
+    private static FileWriter userFw;
+
     /**
-     *
      * @param f
      * @param charset
      * @return
      * @throws IOException
      */
     public static List<String> getStringFromFile(File f, String charset) throws IOException {
-        ArrayList<String> lines = new ArrayList<String>() ;
+        ArrayList<String> lines = new ArrayList<String>();
         FileInputStream fis = new FileInputStream(f);
         InputStreamReader isr = new InputStreamReader(fis, charset);
         BufferedReader br = new BufferedReader(isr);
         String line = null;
         while ((line = br.readLine()) != null) {
-            lines.add(line) ;
+            lines.add(line);
         }
         br.close();
         isr.close();
         fis.close();
-        return lines ;
+        return lines;
     }
-    public static void writwLog(File f,String content){
+
+    public static void writeUser(String content) {
         try {
-            FileWriter fw = new FileWriter(f,true) ;
-            BufferedWriter bw = new BufferedWriter(fw) ;
-            bw.write(content);
-            bw.close();
-            fw.close();
+            if (userFw == null) {
+                userFw = new FileWriter(new File(fileUser));
+            }
+            userFw.write(content);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void writeLog(String content) {
+        try {
+            if (logFw == null) {
+                logFw = new FileWriter(new File(fileLog));
+            }
+            logFw.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close() {
+        try {
+            logFw.close();
+            userFw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
